6ad85213fd758f0fece72cadfdd9a771ef647d68
Amiel De Los Reyes <amielxvr@gmail.com>
2025-01-10 13:17:52 +0800

    created app-registry service

---
+++ app-registry/src/main/java/amiel_aljon/app_registry/AppRegistryApplication.java
@@ -0,0 +1,17 @@
+package amiel_aljon.app_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+//@EnableConfigServer
+public class AppRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRegistryApplication.class, args);
+	}
+
+}
