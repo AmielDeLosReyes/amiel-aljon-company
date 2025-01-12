adac830db458b84ba3ee3b0db2ed193349de34d7
Amiel De Los Reyes <amielxvr@gmail.com>
2025-01-12 12:37:25 +0800

    initial code for api-gateway

---
+++ api-gateway/src/test/java/amiel_aljon/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package amiel_aljon.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
