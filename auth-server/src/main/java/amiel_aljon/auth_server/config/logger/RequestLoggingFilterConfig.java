1f42d63d5b82cb0d589c004d73db76790e48ef19
Amiel Xavier De Los Reyes <amielxavierdelosreyes@Amiels-MacBook-Pro.local>
2025-01-30 06:43:02 +0800

    added logging and logback configurations

---
+++ auth-server/src/main/java/amiel_aljon/auth_server/config/logger/RequestLoggingFilterConfig.java
@@ -0,0 +1,19 @@
+package amiel_aljon.auth_server.config.logger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+}
