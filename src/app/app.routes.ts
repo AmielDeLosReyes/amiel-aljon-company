43d8999b130f165beb88d98b6d5318675357b0e6
Amiel Xavier De Los Reyes <amielxavierdelosreyes@Amiels-MacBook-Pro.local>
2025-01-30 12:51:22 +0800

    Added super admin login static page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SuperAdminLoginComponent } from './pages/super-admin-login/super-admin-login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'super-admin-login', component: SuperAdminLoginComponent },
+
+];
