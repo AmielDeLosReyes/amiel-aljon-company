a383092b7412765df05a5487a4a5d1fb9ff349e4
Amiel Xavier De Los Reyes <amielxavierdelosreyes@Amiels-MacBook-Pro.local>
2025-01-30 12:51:22 +0800

    Added super admin login static page

---
+++ src/app/pages/super-admin-login/super-admin-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-super-admin-login',
+  imports: [],
+  templateUrl: './super-admin-login.component.html',
+  styleUrl: './super-admin-login.component.css'
+})
+export class SuperAdminLoginComponent {
+
+}
